d0842b16d76078afd460b521d5174dd8112b12dd
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-09 16:16:22 -0600

    finished search bar

--- react-app/src/components/Navigation/AuthNavigation.js
+++ react-app/src/components/Navigation/AuthNavigation.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { findCrypto } from "../../store/crypto";
@@ -6,32 +6,104 @@ import "./AuthNavigation.css";
 
 const AuthNavigation = () => {
   const results = useRef(null);
+  const searchBar = useRef(null);
   const dispatch = useDispatch();
   const [search, setSearch] = useState();
   const searchResults = useSelector((state) => state.crypto.searchRes);
+  const regex = new RegExp(search, "gi");
+
+  const hide = (e) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      results.current.classList.add("hidden");
+      searchBar.current.style.backgroundColor = "black";
+      searchBar.current.style.borderBottom = "0.5px solid grey";
+    } else {
+      searchBar.current.style.backgroundColor = "rgb(42, 47, 51)";
+      searchBar.current.style.borderBottom = "none";
+    }
+  };
+
+  const show = (e) => {
+    searchBar.current.style.backgroundColor = "rgb(42, 47, 51)";
+    if (e.target.value.length > 0) {
+      results.current.classList.remove("hidden");
+      searchBar.current.style.borderBottom = "none";
+    } else {
+      searchBar.current.style.borderBottom = "0.5px solid grey";
+    }
+  };
+
+  const colorChange = () => {
+    if (results.current.classList.contains("hidden")) {
+      searchBar.current.style.backgroundColor = "black"
+    }
+  }
 
   useEffect(() => {
     dispatch(findCrypto(search));
+
+    if (search?.length > 0) {
+      results.current.classList.remove("hidden");
+      searchBar.current.style.borderBottom = "none";
+    } else {
+      results.current.classList.add("hidden");
+      searchBar.current.style.borderBottom = "0.5px solid grey";
+    }
   }, [search, dispatch]);
 
   return (
-    <div className="nav-main">
-      <img className="nav-coin" src="https://thumbs.gfycat.com/SkinnyAccomplishedBoa-size_restricted.gif" />
+    <div className="nav-main" onBlur={(e) => hide(e)}>
+      <img
+        className="nav-coin"
+        src="https://thumbs.gfycat.com/SkinnyAccomplishedBoa-size_restricted.gif"
+      />
       <div className="search-container">
         <input
           placeholder="Search"
           className="search-bar"
           onChange={(e) => setSearch(e.target.value)}
+          onFocus={(e) => show(e)}
+          ref={searchBar}
+          onMouseEnter={() =>
+            (searchBar.current.style.backgroundColor = "rgb(42, 47, 51)")
+          }
+          onMouseLeave={colorChange}
         />
-        <div className="search-results" ref={results}>
-          {searchResults?.length > 0 && search?.length > 0
-            ? searchResults.map((result, i) => (
-                <div key={i}>
-                  <div>{result.symbol}</div>
-                  <div>{result.name}</div>
-                </div>
-              ))
-            : null}
+        <div
+          className="search-results hidden"
+          tabIndex={0}
+          ref={results}
+          onMouseEnter={() =>
+            (searchBar.current.style.backgroundColor = "rgb(42, 47, 51)")
+          }
+        >
+          {searchResults?.length > 0 &&
+            search?.length > 0 &&
+            searchResults.map((result, i) => (
+              <div key={i} className="result">
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: result.symbol.replace(
+                      regex,
+                      (match) => `<span>${match}</span>`
+                    ),
+                  }}
+                ></div>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: result.name.replace(
+                      regex,
+                      (match) => `<span>${match}</span>`
+                    ),
+                  }}
+                ></div>
+              </div>
+            ))}
+          {searchResults?.length === 0 && search?.length > 0 && (
+            <div className="result">
+              We were unable to find any results for your search.
+            </div>
+          )}
         </div>
       </div>
       <div className="right-nav">
