527b2b01f0de1def78907e13efbeb92665764874
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-10 21:20:58 -0600

    added an extra route

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -4,8 +4,14 @@ from app.models import db, Watchlist
 
 watchlist_routes = Blueprint('watchlist', __name__)
 
-@watchlist_routes.route('/<int:user_id>')
+@watchlist_routes.route('/user/<int:user_id>')
 def get_watchlist(user_id):
     watchlist = Watchlist.query.filter_by(user_id=user_id).all()
 
-    return {'watchlists': [item for item in watchlist]}
+    return {'watchlists': [item.to_dict() for item in watchlist]}
+
+@watchlist_routes.route('/<int:watchlist_id>')
+def populate_watchlist(watchlist_id):
+    watchlist = Watchlist_Crypto.query.filter_by(watchlist_id=watchlist_id).all()
+
+    return {'cryptos': [item.to_dict()["crypto_id"] for item in watchlist]}
