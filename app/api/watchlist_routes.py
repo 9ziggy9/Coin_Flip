3f20d3ed870e0a583657bedf24a0ea451174ca69
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-11 22:38:03 -0600

    fixed watchlist get route to asc order

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -6,7 +6,7 @@ watchlist_routes = Blueprint('watchlist', __name__)
 
 @watchlist_routes.route('/user/<int:user_id>')
 def get_watchlist(user_id):
-    watchlist = Watchlist.query.filter_by(user_id=user_id).all()
+    watchlist = Watchlist.query.filter_by(user_id=user_id).order_by(Watchlist.id.asc()).all()
 
     return {'watchlists': [item.to_dict() for item in watchlist]}
 
