d588507d43e530706e101f609cc3ead4196dcb3b
Victor Hou <hou_v@yahoo.com>
2021-11-11 20:32:03 -0800

    worked on transactions component

--- react-app/src/components/Transactions/Transaction.js
+++ react-app/src/components/Transactions/Transaction.js
@@ -10,7 +10,7 @@ const Transactions = () => {
     const user = useSelector(state => state.session.user)
     const portfolios = useSelector(state => Object.values(state.portfolio))
     const transactions = useSelector(state => Object.values(state.transaction))
-    const cryptos = useSelector(state => Object.values(state.crypto))
+    // const cryptos = useSelector(state => Object.values(state.crypto))
 
     const [cryptoId, setCryptoId] = useState(0);
     const [transactionsArr, setTransactionsArr] = useState();
@@ -18,6 +18,7 @@ const Transactions = () => {
     useEffect(() => {
         dispatch(userPortfolios(user?.id))
         dispatch(getUserTransactions(user?.id))
+        // dispatch(getAllCrypto());
     }, [dispatch]);
 
     useEffect(() => {
@@ -35,24 +36,49 @@ const Transactions = () => {
                     </div>
                     <ul className="user_crypto_list">
                         {portfolios ? portfolios.map(portfolio => (
-                            <li key={portfolio.id}>
+                            <li className="each_crypto_li"key={portfolio.id}>
                                 <div className="crypto_name_container" id={portfolio.crypto_id} value={portfolio.id} onClick={(e) => setCryptoId(portfolio.crypto_id)}>
-                                    <h3 className="crypto_name_text" value={portfolio.id}>{portfolio.crypto_id}</h3>
+                                    <div className="crypto_name_label_container" value={portfolio.id}>
+                                        <h3 className="crypto_name">Crypto {portfolio.crypto_id}</h3>
+                                    </div>
                                 </div>
                             </li>
                         )): ""}
                     </ul>
                 </div>
                 <div className="transaction_container_right">
-                    <ul>
+                    <div className="transaction_title_container">
+                        <p className="transaction_title">Transaction</p>
+                    </div>
+                    <ul className="transaction_list_container">
                         {transactionsArr && transactionsArr?.map(transaction => (
-                            <li key={transaction.id}>
-                                <div>
-                                    {transaction.id}
+                            <li className="transaction_li"key={transaction.id}>
+                                <div className="transaction_details_area">
+                                    <div className="transaction_details_container">
+                                        <div className="transaction_details_container">
+                                            <h5 className="transaction_details_label">Transaction ID</h5>
+                                            <h3 className="transaction_details_value">{transaction.id}</h3>
+                                        </div>
+                                    </div>
+                                    <div className="transaction_details">
+                                        <div className="transaction_details_container">
+                                            <h5 className="transaction_details_label">Price</h5>
+                                            <h3 className="transaction_details_value">{`\$${transaction.price}`}</h3>
+                                        </div>
+                                    </div>
+                                    <div className="transaction_details">
+                                        <div className="transaction_details_container">
+                                            <h5 className="transaction_details_label">Qty.</h5>
+                                            <h3 className="transaction_details_value">{transaction.quantity}</h3>
+                                        </div>
+                                    </div>
+                                    <div className="transaction_details">
+                                        <div className="transaction_details_container">
+                                            <h5 className="transaction_details_label">Transaction Type</h5>
+                                            <h3 className="transaction_details_value">{transaction.type.toUpperCase()}</h3>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div>{transaction.price}</div>
-                                <div>{transaction.quantity}</div>
-                                <div>{transaction.type}</div>
                             </li>
                         ))}
                     </ul>
