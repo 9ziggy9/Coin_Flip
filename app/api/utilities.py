dfac0ff9ca208ec86efc94c3ddf40a2f5de374a2
Ziggy <davidarogers@protonmail.com>
2021-11-13 18:27:05 -0800

    preparing utility function to return yearly prices

--- app/api/utilities.py
+++ app/api/utilities.py
@@ -9,6 +9,6 @@ def datetime_to_unix(year, month, day):
     timestamp = (dt - datetime(1970, 1, 1)).total_seconds()
     return timestamp
 
- def unix_to_datetime(unix_time)
-     ts = int(unix_time/1000 if
-     return datetime.utcfromtime
+def unix_to_datetime(unix_time):
+    ts = int(unix_time/1000 if len(str(unix_time)) > 10 else unix_time)
+    return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %l:%M%p').lower()
