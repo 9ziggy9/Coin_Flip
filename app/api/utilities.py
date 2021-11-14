143b9584c7148ff5a622d6ef8b861be77b5db45b
Ziggy <davidarogers@protonmail.com>
2021-11-13 18:28:05 -0800

    adding utility functions to handle time stamps

--- app/api/utilities.py
+++ app/api/utilities.py
@@ -12,3 +12,14 @@ def datetime_to_unix(year, month, day):
 def unix_to_datetime(unix_time):
     ts = int(unix_time/1000 if len(str(unix_time)) > 10 else unix_time)
     return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %l:%M%p').lower()
+
+def get_yearly_prices(coin):
+    year = int(datetime.now().strftime('%Y'))
+    month = int(datetime.now().strftime('%m'))
+    day = int(datetime.now().strftime('%d'))
+    return cg.get_coin_market_chart_range_by_id(
+        id=coin,
+        vs_currency='usd',
+        from_timestamp=datetime_to_unix(year-1, month, day),
+        to_timestamp=datetime_to_unix(year, month, day)
+    )
