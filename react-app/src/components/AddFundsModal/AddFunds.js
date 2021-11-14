b08c1ecc76bd4fe4eff2702b2ca4cd887c1924b2
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-14 03:34:25 -0600

    add funds complete

--- react-app/src/components/AddFundsModal/AddFunds.js
+++ react-app/src/components/AddFundsModal/AddFunds.js
@@ -32,6 +32,7 @@ const AddFunds = () => {
     }
 
     dispatch(addFunds(number));
+    setBool(false);
   };
 
   const changeAmount = (e) => {
@@ -97,6 +98,7 @@ const AddFunds = () => {
     <div className="add-funds-main">
       <div className="add-close" onClick={() => setBool(false)}>
         <img
+          className="add-img"
           alt="svgImg"
           src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4Igp3aWR0aD0iMjQiIGhlaWdodD0iMjQiCnZpZXdCb3g9IjAgMCAyNCAyNCIKc3R5bGU9IiBmaWxsOiNmZmZmZmY7Ij48cGF0aCBkPSJNIDQuNzA3MDMxMiAzLjI5Mjk2ODggTCAzLjI5Mjk2ODggNC43MDcwMzEyIEwgMTAuNTg1OTM4IDEyIEwgMy4yOTI5Njg4IDE5LjI5Mjk2OSBMIDQuNzA3MDMxMiAyMC43MDcwMzEgTCAxMiAxMy40MTQwNjIgTCAxOS4yOTI5NjkgMjAuNzA3MDMxIEwgMjAuNzA3MDMxIDE5LjI5Mjk2OSBMIDEzLjQxNDA2MiAxMiBMIDIwLjcwNzAzMSA0LjcwNzAzMTIgTCAxOS4yOTI5NjkgMy4yOTI5Njg4IEwgMTIgMTAuNTg1OTM4IEwgNC43MDcwMzEyIDMuMjkyOTY4OCB6Ij48L3BhdGg+PC9zdmc+"
         />
@@ -106,16 +108,19 @@ const AddFunds = () => {
           errors?.map((err) => <div className="add-error">• {err}</div>)}
         <h2 className="add-title">Add Funds</h2>
         <div className="add-inpt">
-        <div className="add-amount">Amount</div>
-        <input
-          autoComplete="off"
-          autoFocus="on"
-          type="text"
-          value={amount}
-          placeholder="$0.00"
-          onChange={(e) => changeAmount(e)}
-          className="add-input"
-        />
+          <div className="add-amount">
+            Amount{" "}
+            <span className="add-warning">(Total Cash Limit: $50,000,000)</span>
+          </div>
+          <input
+            autoComplete="off"
+            autoFocus="on"
+            type="text"
+            value={amount}
+            placeholder="$0.00"
+            onChange={(e) => changeAmount(e)}
+            className="add-input"
+          />
         </div>
         <button onClick={submit} className="add-submit">
           Submit
