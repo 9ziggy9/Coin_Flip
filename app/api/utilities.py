fb83f6f2214678a99c1b33781e2ce112b44411b6
Ziggy <davidarogers@protonmail.com>
2021-11-13 18:24:23 -0800

    adding utility functions to handle time stamps

--- app/api/utilities.py
+++ app/api/utilities.py
@@ -8,3 +8,7 @@ def datetime_to_unix(year, month, day):
     dt = datetime(year, month, day)
     timestamp = (dt - datetime(1970, 1, 1)).total_seconds()
     return timestamp
+
+ def unix_to_datetime(unix_time)
+     ts = int(unix_time/1000 if
+     return datetime.utcfromtime
