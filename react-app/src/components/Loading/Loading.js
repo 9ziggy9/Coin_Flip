c1e6d2a0b6d57f56e9fc15417caadcfe643fe78e
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-14 07:14:37 -0600

    removing stuff

--- react-app/src/components/Loading/Loading.js
+++ react-app/src/components/Loading/Loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="loading-main">
-      <img className="centered" img src="https://i.gifer.com/J0fF.gif" />{" "}
+      <img className="centered" img src="https://i.gifer.com/J0fF.gif" />
     </div>
   );
 }
